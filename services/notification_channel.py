c1456c97fb9b50f441da672c74082810efec411e
Rashad Musayev <rmusayevr19@gmail.com>
2024-08-03 16:31:35 +0400

    add notification method for services

---
+++ services/notification_channel.py
@@ -0,0 +1,18 @@
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+
+# Trigger WebSocket notification
+def get_notification(notification):
+    # Trigger WebSocket notification
+    channel_layer = get_channel_layer()
+    group_name = f"user_{notification.recipient.id}"
+    async_to_sync(channel_layer.group_send)(
+        group_name,
+        {
+            "type": "send_notification",
+            "message": notification.message,
+            "notification_type": notification.notification_type,
+            "product_id": notification.product_id.pk if notification.product_id else None,
+        }
+    )
