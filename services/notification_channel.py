3c06012c547fb5e110de4a4c35c72738d1274057
Rashad Musayev <rmusayevr19@gmail.com>
2024-08-07 01:23:05 +0400

    add firebase settings and update channel response data

--- services/notification_channel.py
+++ services/notification_channel.py
@@ -3,7 +3,7 @@ from asgiref.sync import async_to_sync
 
 
 # Trigger WebSocket notification
-def get_notification(notification):
+def trigger_notification(notification):
     # Trigger WebSocket notification
     channel_layer = get_channel_layer()
     group_name = f"user_{notification.recipient.id}"
@@ -14,5 +14,12 @@ def get_notification(notification):
             "message": notification.message,
             "notification_type": notification.notification_type,
             "product_id": notification.product_id.pk if notification.product_id else None,
+            "sender": {
+                    "id": notification.sender.pk,
+                    "username": notification.sender.username,
+                    "first_name": notification.sender.first_name,
+                    "last_name": notification.sender.last_name,
+                    "avatar": notification.sender.avatar,
+            }
         }
     )
