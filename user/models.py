cca38ead42c1b4bce264194c306100a9f9cfb592
Rashad Musayev <rmusayevr19@gmail.com>
2024-08-16 12:50:42 +0400

    prevent multiple login

--- user/models.py
+++ user/models.py
@@ -210,3 +210,13 @@ class Messages(DateMixin):
 
     def __str__(self):
         return f"{self.user.username}'s chat messages with {self.chat.to_user}"
+
+
+class LoggedInUser(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='logged_in_user')
+    # Session keys are 32 characters long
+    session_key = models.CharField(max_length=32, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name()
--- user/signals.py
+++ user/signals.py
@@ -1,10 +1,12 @@
-from django.db.models.signals import post_save
 from django.conf import settings
+from django.contrib.auth import user_logged_in, user_logged_out
+from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import OTPToken
+from .models import OTPToken, LoggedInUser
 from .utils.otp import generate_otp
 from django.core.mail import send_mail
 from django.utils import timezone
+
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_token(sender, instance, created, **kwargs):
@@ -33,3 +35,13 @@ def create_token(sender, instance, created, **kwargs):
                 receiver,
                 fail_silently=False,
             )
+
+
+@receiver(user_logged_in)
+def on_user_logged_in(sender, request, **kwargs):
+    LoggedInUser.objects.get_or_create(user=kwargs.get('user'))
+
+
+@receiver(user_logged_out)
+def on_user_logged_out(sender, **kwargs):
+    LoggedInUser.objects.filter(user=kwargs.get('user')).delete()
